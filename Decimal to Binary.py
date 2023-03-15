ba714df05d783f3963117f3c27bd2fd914d2cbd4
Dashfire <marc.czapp@web.de>
2023-03-15 15:46:36 +0100

    Update Decimal to Binary.py

--- Decimal to Binary.py
+++ Decimal to Binary.py
@@ -2,22 +2,20 @@ binaryNumber = '1100'
 binary = list(binaryNumber)
 binary.reverse()
 
-def DecimalToBinary(DeciNumber,NumberofBits):
+def DecimalToBinary(deciNumber,amountOfBits):
     binaryOutput = []
-    for i in range(NumberofBits):
-        helpNumber = DeciNumber % 2
-        DeciNumber = DeciNumber // 2
-        NumbertoString = str(helpNumber)
-        binaryOutput.append(NumbertoString)
+    for i in range(amountOfBits):
+        helpNumber = deciNumber % 2
+        deciNumber = deciNumber // 2
+        numberToString = str(helpNumber)
+        binaryOutput.append(numberToString)
     binaryOutput.reverse()
     return ''.join(binaryOutput)
 
 
-def BinaryToDecimal(BinNumber):
+def BinaryToDecimal(binNumber):
     decimalNumber = 0
-    for i in range(len(BinNumber)):
-        helpNumber = decimalNumber + int(BinNumber[i]) * (2**i)
+    for i in range(len(binNumber)):
+        helpNumber = decimalNumber + int(binNumber[i]) * (2**i)
         decimalNumber = helpNumber
     return decimalNumber
-
-print (DecimalToBinary(15,16))
