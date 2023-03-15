7b87515acee13ad2aa805d5a2ec57b6a5a69cc0b
Dashfire <marc.czapp@web.de>
2023-03-15 02:07:19 +0100

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+def Fibo1(x):
+    if x == 1 or x == 2:
+        return 1
+    else:
+        return Fibo1(x-1) + Fibo1(x-2)
+    
+def Fibo2(x):
+    Num1 = 0
+    Num2 = 1
+    for i in range(x-1):
+        Num3 = Num1 + Num2
+        Num1 = Num2
+        Num2 = Num3
+    return Num3
+
+print(Fibo1(11))
